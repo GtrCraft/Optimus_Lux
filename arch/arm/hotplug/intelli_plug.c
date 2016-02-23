c08c6ad18550ad6fd81da836c8ec5773437af53c
desaishivam26 <shivamdesaixda@gmail.com>
2016-02-23 19:33:50 +0530

    intelli_plug: define thread capacity for msm8916
    This enables auto calculation for moto msm8916 devices. Moto msm8916 devices use Cortex A7 flag so define it accordingly

--- arch/arm/hotplug/intelli_plug.c
+++ arch/arm/hotplug/intelli_plug.c
@@ -89,7 +89,7 @@ module_param(screen_off_max, uint, 0664);
 #elif defined(CONFIG_ARCH_MSM8960) || defined(CONFIG_ARCH_APQ8064) || \
 defined(CONFIG_ARCH_MSM8974)
 #define THREAD_CAPACITY	(339 - CAPACITY_RESERVE)
-#elif defined(CONFIG_ARCH_MSM8226) || defined (CONFIG_ARCH_MSM8926) || \
+#elif defined(CONFIG_ARCH_MSM8226) || defined (CONFIG_ARCH_MSM8916) || defined (CONFIG_ARCH_MSM8926) || \
 defined (CONFIG_ARCH_MSM8610) || defined (CONFIG_ARCH_MSM8228)
 #define THREAD_CAPACITY (190 - CAPACITY_RESERVE)
 #else
