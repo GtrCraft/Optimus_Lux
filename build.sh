desaishivam26/Optimus_Osprey

build: release 18

build.sh
@@ -13,7 +13,7 @@ KERNEL_DEFCONFIG=osprey_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
 JOBS=8
 ANY_KERNEL2_DIR=$KERNEL_DIR/Anykernel2/
-FINAL_KERNEL_ZIP=Optimus-R17-Osprey.zip
+FINAL_KERNEL_ZIP=Optimus-R18-Osprey.zip
 
 # Clean build always lol
 echo "**** Cleaning ****"

GtrCraft/Optimus_Lux

build.sh
@@ -13,7 +13,7 @@ KERNEL_DEFCONFIG=lux_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
 JOBS=4
 ANY_KERNEL2_DIR=$KERNEL_DIR/Anykernel2
-FINAL_KERNEL_ZIP=Optimus-R17-Lux.zip
+FINAL_KERNEL_ZIP=Optimus-R18-Lux.zip
 
 # Clean build always lol
 echo "**** Cleaning ****"