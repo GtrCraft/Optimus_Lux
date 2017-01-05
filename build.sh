0c391157db862830f00d980933547282a4b292b4
GtrCraft <nickvanbokhorst@hotmail.com>
2017-01-05 15:09:31 +0100

    Add everything for lux
    Also updating to R19

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@
 ### Shivam Desai (shivamdesaixda@gmail.com)
 ### A custom build script to build zImage,DTB & wlan module (Anykernel2 method)
 
-set -e
+#set -e
 
 ## Copy this script inside the kernel directory
-KERNEL_DIR=$PWD
-KERNEL_TOOLCHAIN=/home/shivam/x-tools/arm-cortex_a53-linux-gnueabihf/bin/arm-eabi-
-KERNEL_DEFCONFIG=osprey_defconfig
+KERNEL_DIR=/home/nickv/data/optimus/kernel
+KERNEL_TOOLCHAIN=/home/nickv/data/optimus/prebuilts/arm-eabi-4.9/bin/arm-eabi-
+KERNEL_DEFCONFIG=lux_defconfig
 DTBTOOL=$KERNEL_DIR/Dtbtool/
-JOBS=8
-ANY_KERNEL2_DIR=$KERNEL_DIR/Anykernel2/
-FINAL_KERNEL_ZIP=Optimus-R19-Osprey.zip
+JOBS=16
+ANY_KERNEL2_DIR=$KERNEL_DIR/Anykernel2
+FINAL_KERNEL_ZIP=Optimus-R19-Lux.zip
 
 # Clean build always lol
 echo "**** Cleaning ****"
@@ -55,8 +55,8 @@ cp $KERNEL_DIR/drivers/staging/prima/wlan.ko $ANY_KERNEL2_DIR/modules/
 echo "**** Time to zip up! ****"
 cd $ANY_KERNEL2_DIR/
 zip -r9 $FINAL_KERNEL_ZIP * -x README $FINAL_KERNEL_ZIP
-rm -rf /home/shivam/$FINAL_KERNEL_ZIP
-cp /home/shivam/optimus/Anykernel2/$FINAL_KERNEL_ZIP /home/shivam/$FINAL_KERNEL_ZIP
+rm -rf /home/gtrcraft/data/optimus/$FINAL_KERNEL_ZIP
+cp /home/nickv/data/optimus/kernel/Anykernel2/$FINAL_KERNEL_ZIP /home/nickv/data/optimus/$FINAL_KERNEL_ZIP
 
 echo "**** Good Bye!! ****"
 cd $KERNEL_DIR
@@ -64,4 +64,3 @@ rm -rf arch/arm/boot/dtb
 rm -rf $ANY_KERNEL2_DIR/$FINAL_KERNEL_ZIP
 rm -rf Anykernel2/zImage
 rm -rf Anykernel2/dtb
-
